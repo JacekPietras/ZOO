4f5889f3449c98657a0778df012688ccad1f7bbc
Jacek Pietras <jock69pl@gmail.com>
2021-02-17 22:34:54 +0100

    leak hunt

--- app/src/main/java/com/jacekpietras/zoo/app/MainActivity.kt
+++ app/src/main/java/com/jacekpietras/zoo/app/MainActivity.kt
@@ -15,6 +15,7 @@ import com.jacekpietras.zoo.core.binding.viewBinding
 import com.jacekpietras.zoo.databinding.ActivityMainBinding
 import com.jacekpietras.zoo.tracking.TrackingService
 import com.jacekpietras.zoo.tracking.isServiceRunning
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,20 +29,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         // https://issuetracker.google.com/issues/139738913
-        if (isTaskRoot
-            && supportFragmentManager.primaryNavigationFragment
-                ?.childFragmentManager?.backStackEntryCount == 0
-            && supportFragmentManager.backStackEntryCount == 0
-            && SDK_INT == VERSION_CODES.Q
-        ) {
+        if (isTaskRoot && isBackStackEmpty && SDK_INT == VERSION_CODES.Q) {
             finishAfterTransition()
         } else {
             super.onBackPressed()
         }
     }
 
+    override fun onStop() {
+        // https://issuetracker.google.com/issues/139738913
+        Timber.e("leak hunt pause root:$isTaskRoot finishing:$isFinishing")
+        if (isTaskRoot && SDK_INT == VERSION_CODES.Q && isFinishing) {
+            finishAfterTransition()
+            if (!isServiceRunning(this, TrackingService::class.java)) {
+                killProcess(Process.myPid())
+            }
+        }
+        super.onStop()
+    }
+
     override fun onDestroy() {
         // https://issuetracker.google.com/issues/139738913
+        Timber.e("leak hunt destroy root:$isTaskRoot")
         if (isTaskRoot && SDK_INT == VERSION_CODES.Q) {
             finishAfterTransition()
             if (!isServiceRunning(this, TrackingService::class.java)) {
@@ -50,6 +59,11 @@ class MainActivity : AppCompatActivity() {
         }
         super.onDestroy()
     }
+
+    private val isBackStackEmpty
+        get() = supportFragmentManager.primaryNavigationFragment
+            ?.childFragmentManager?.backStackEntryCount == 0
+                && supportFragmentManager.backStackEntryCount == 0
 
     private fun setViews() {
         binding.navView.setupWithNavController(findNavController())
