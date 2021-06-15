128fd2c63e71dccae8061ed22a6f9edca2e715ca
Jacek Pietras <jock69pl@gmail.com>
2021-06-15 22:02:03 +0200

    icons

--- presentation-catalogue/src/main/java/com/jacekpietras/zoo/catalogue/feature/list/model/AnimalDivision.kt
+++ presentation-catalogue/src/main/java/com/jacekpietras/zoo/catalogue/feature/list/model/AnimalDivision.kt
@@ -6,11 +6,11 @@ import com.jacekpietras.zoo.catalogue.R
 enum class AnimalDivision(
      @DrawableRes val iconRes: Int,
 ) {
-    MAMMAL(R.drawable.ic_dickbutt_24dp),
-    BIRD(R.drawable.ic_dickbutt_24dp),
-    AMPHIBIAN(R.drawable.ic_dickbutt_24dp),
-    REPTILE(R.drawable.ic_dickbutt_24dp),
-    FISH(R.drawable.ic_dickbutt_24dp),
-    ARTHROPOD(R.drawable.ic_dickbutt_24dp),
-    MOLLUSCA(R.drawable.ic_dickbutt_24dp),
+    MAMMAL(R.drawable.ic_lion),
+    BIRD(R.drawable.ic_bird),
+    AMPHIBIAN(R.drawable.ic_frog),
+    REPTILE(R.drawable.ic_snake),
+    FISH(R.drawable.ic_fish),
+    ARTHROPOD(R.drawable.ic_spider),
+    MOLLUSCA(R.drawable.ic_snail),
 }
