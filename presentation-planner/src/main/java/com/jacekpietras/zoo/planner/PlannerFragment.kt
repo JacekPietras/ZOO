3c58843e8cece81005802b52214d467f6f96139f
Jacek Pietras <jock69pl@gmail.com>
2021-11-07 19:43:41 +0100

    planner content in compose

--- presentation-planner/src/main/java/com/jacekpietras/zoo/planner/PlannerFragment.kt
+++ presentation-planner/src/main/java/com/jacekpietras/zoo/planner/PlannerFragment.kt
@@ -1,5 +1,27 @@
 package com.jacekpietras.zoo.planner
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import com.google.android.material.composethemeadapter.MdcTheme
 
-class PlannerFragment : Fragment(R.layout.fragment_planner)
+class PlannerFragment : Fragment() {
+
+    override fun onCreateView(i: LayoutInflater, c: ViewGroup?, s: Bundle?): View = ComposeView(requireContext()).apply {
+        setContent {
+            MdcTheme {
+                Text(
+                    text = "Idź do najbliższej toalety/palarni/jedzenia\nDodaj trase predefiniowaną\nDodaj obiad o danej godzinie i miejscu\nDodaj karmienie które chcesz zobaczyć\nPrzegląd trasy z godzinami",
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+                )
+            }
+        }
+    }
+}
