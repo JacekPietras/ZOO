36ff908bed1290819cbaacd464715118cee1486a
Jacek Pietras <jock69pl@gmail.com>
2021-11-07 18:05:47 +0100

    Cutting fragment into pieces

--- presentation-catalogue/src/main/java/com/jacekpietras/zoo/catalogue/feature/list/ui/CatalogueFragment.kt
+++ presentation-catalogue/src/main/java/com/jacekpietras/zoo/catalogue/feature/list/ui/CatalogueFragment.kt
@@ -6,14 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.jacekpietras.zoo.catalogue.feature.list.model.AnimalDivision
-import com.jacekpietras.zoo.catalogue.feature.list.model.CatalogueListItem
 import com.jacekpietras.zoo.catalogue.feature.list.model.CatalogueViewState
 import com.jacekpietras.zoo.catalogue.feature.list.router.CatalogueRouterImpl
 import com.jacekpietras.zoo.catalogue.feature.list.viewmodel.CatalogueViewModel
@@ -29,53 +26,28 @@ class CatalogueFragment : Fragment() {
             val viewState: CatalogueViewState by viewModel.viewState.collectAsState(initial = CatalogueViewState())
 
             with(viewState) {
-                BindFragment(
-                    filterList,
-                    filtersVisible,
-                    searchVisible,
-                    searchText,
-                    onSearch = viewModel::onSearch,
-                    onSearchClicked = viewModel::onSearchClicked,
-                    onFilterClicked = viewModel::onFilterClicked,
-                    animalList = animalList,
-                    onAnimalClicked = { animalId ->
-                        viewModel.onAnimalClicked(
-                            animalId = animalId,
-                            router = router
+                MaterialTheme {
+                    Column {
+                        ToolbarWithFilters(
+                            filterList,
+                            filtersVisible,
+                            searchVisible,
+                            searchText,
+                            onSearch = viewModel::onSearch,
+                            onSearchClicked = viewModel::onSearchClicked,
+                            onFilterClicked = viewModel::onFilterClicked,
                         )
-                    },
-                )
-            }
-        }
-    }
-
-    @Composable
-    private fun BindFragment(
-        filterList: List<AnimalDivision>,
-        filtersVisible: Boolean,
-        searchVisible: Boolean,
-        searchText: String,
-        animalList: List<CatalogueListItem>,
-        onSearch: (String) -> Unit,
-        onSearchClicked: () -> Unit,
-        onFilterClicked: (AnimalDivision) -> Unit,
-        onAnimalClicked: (animalId: String) -> Unit,
-    ) {
-        MaterialTheme {
-            Column {
-                ToolbarWithFilters(
-                    filterList = filterList,
-                    filtersVisible = filtersVisible,
-                    searchVisible = searchVisible,
-                    searchText = searchText,
-                    onSearch = onSearch,
-                    onSearchClicked = onSearchClicked,
-                    onFilterClicked = onFilterClicked,
-                )
-                AnimalList(
-                    animalList = animalList,
-                    onAnimalClicked = onAnimalClicked,
-                )
+                        AnimalList(
+                            animalList = animalList,
+                            onAnimalClicked = { animalId ->
+                                viewModel.onAnimalClicked(
+                                    animalId = animalId,
+                                    router = router
+                                )
+                            },
+                        )
+                    }
+                }
             }
         }
     }
