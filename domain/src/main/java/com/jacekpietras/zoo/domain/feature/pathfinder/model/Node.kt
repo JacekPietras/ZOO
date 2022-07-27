fb47593dfa9350df305db247c0fda146a26421fd
Jacek Pietras <jock69pl@gmail.com>
2022-07-27 23:38:09 +0200

    possible crash

--- domain/src/main/java/com/jacekpietras/zoo/domain/feature/pathfinder/model/Node.kt
+++ domain/src/main/java/com/jacekpietras/zoo/domain/feature/pathfinder/model/Node.kt
@@ -3,7 +3,7 @@ package com.jacekpietras.zoo.domain.feature.pathfinder.model
 import com.jacekpietras.geometry.PointD
 import com.jacekpietras.geometry.haversine
 
-internal data class Node(
+internal class Node(
     val point: PointD,
     val edges: MutableSet<Edge> = mutableSetOf()
 ) {
@@ -14,11 +14,11 @@ internal data class Node(
     val y: Double
         get() = point.y
 
-    fun connect(node: Node, technical: Boolean, backward:Boolean) {
+    fun connect(node: Node, technical: Boolean, backward: Boolean) {
         edges.add(Edge(node, technical, backward))
     }
 
-    fun connectAndCalc(node: Node, technical: Boolean, backward:Boolean) {
+    fun connectAndCalc(node: Node, technical: Boolean, backward: Boolean) {
         edges.add(Edge(node, technical, backward, haversine(this.x, this.y, node.x, node.y)))
     }
 
@@ -26,5 +26,19 @@ internal data class Node(
         edges.remove(edges.first { it.node == node })
     }
 
+    override fun hashCode(): Int =
+        point.hashCode()
+
     override fun toString() = "Node($x, $y),"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Node
+
+        if (point != other.point) return false
+
+        return true
+    }
 }
