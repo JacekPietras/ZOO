ab696742a1db77b17547d2030cba3756ffe4fe47
Jacek Pietras <jock69pl@gmail.com>
2022-08-15 22:43:33 +0200

    color adjustment

--- presentation-core/src/main/java/com/jacekpietras/zoo/core/theme/MapColors.kt
+++ presentation-core/src/main/java/com/jacekpietras/zoo/core/theme/MapColors.kt
@@ -14,8 +14,8 @@ class MapColors(
     val colorAccent = Color(0xFFEF6C00) onNight Color(0xFFD84315)
 
     val colorMapGrass = Color(0xFFD2E7D3) onNight Color(0xFF2F6832)
-    val colorMapForest = Color(0xFF8EBB90) onNight Color(0xFF2D4E2E)
-    val colorMapWater = Color(0xFF2487B9) onNight Color(0xFF215068)
+    val colorMapForest = Color(0xFF9CC49E) onNight Color(0xFF2D4E2E)
+    val colorMapWater = Color(0xFF61AACF) onNight Color(0xFF215068)
 
     val colorMapBuilding = Color(0xFFf1f3f4) onNight Color(0xFF2f3a47)
     val colorMapBuildingBorder = Color(0xFFD7DBDD) onNight Color(0xFF0E141E)
@@ -25,13 +25,13 @@ class MapColors(
     val colorMapRoadVisited = Color(0xFFD1FAE1) onNight Color(0xFF447C43)
     val colorMapTaken = colorPrimary
     val colorMapNavigation = colorAccent
-    val colorMapLines = Color(0xFFC9765D) onNight Color(0xFFD84315)
+    val colorMapLines = Color(0xFFAA834F) onNight Color(0xFF584020)
     val colorMapTechnical = Color(0xFFFFF0F0) onNight Color(0xFF221717)
 
     val colorSmallMapBackground = colorMapForest
-    val colorSmallMapBuilding = Color(0xFF768A7E) onNight Color(0xFF768A7E)
-    val colorSmallMapRoad = Palette.white
-    val colorSmallMapAnimal = Color(0xFFF44336) onNight Color(0xFFF44336)
+    val colorSmallMapBuilding = colorMapBuilding // Color(0xFF768A7E) onNight Color(0xFF768A7E)
+    val colorSmallMapRoad = colorMapRoad // Palette.white
+    val colorSmallMapAnimal = colorMapNavigation // Color(0xFFF44336) onNight Color(0xFFF44336)
 
     private infix fun <T> T.onNight(right: T): T = if (nightTheme) right else this
 }
