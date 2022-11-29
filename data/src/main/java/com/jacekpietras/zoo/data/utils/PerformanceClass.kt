6b9fbe4ab286c756cc417c7c5d7d931193e76e3e
jacekpietras <jpietras@spoton.com>
2022-11-29 23:32:32 +0100

    year class

--- data/src/main/java/com/jacekpietras/zoo/data/utils/PerformanceClass.kt
+++ data/src/main/java/com/jacekpietras/zoo/data/utils/PerformanceClass.kt
@@ -1,25 +1,28 @@
 package com.jacekpietras.zoo.data.utils
 
 import android.content.Context
+import android.os.Build
 import com.facebook.device.yearclass.DeviceInfo
-import timber.log.Timber
 
 class PerformanceClass(context: Context) {
 
-    private var cpuFreq = DeviceInfo.getCPUMaxFreqKHz()
+    private var cpuFreq = DeviceInfo.getCPUMaxFreqKHz() / MHZ_IN_KHZ
     private var numberOfCores = DeviceInfo.getNumberOfCPUCores()
     private var totalMemory = DeviceInfo.getTotalMemory(context).toDouble() / GB
 
     fun getRating(): Boolean {
-        Timber.e("dups cpuFreq $cpuFreq")
-        Timber.e("dups numberOfCores $numberOfCores")
-        Timber.e("dups totalMemory $totalMemory")
+        if (Build.BRAND == "Android") {
+            return true // We have Emulator there
+        }
 
-        return numberOfCores >= 4 && (cpuFreq == -1 || cpuFreq >= 2) && totalMemory >= 1.5
+        return numberOfCores >= 6 &&
+                (cpuFreq < 5 || cpuFreq >= 1800) &&
+                totalMemory >= 2
     }
 
     private companion object {
 
         const val GB = 1024L * 1024L * 1024L
+        const val MHZ_IN_KHZ = 1000
     }
 }
