5a3aae910fa05994727a238677fe82a474fe8204
jacekpietras <jpietras@spoton.com>
2022-12-23 21:14:35 +0100

    dijkstra improvements

--- domain/src/main/java/com/jacekpietras/zoo/domain/feature/pathfinder/Dijkstra.kt
+++ domain/src/main/java/com/jacekpietras/zoo/domain/feature/pathfinder/Dijkstra.kt
@@ -1,9 +1,10 @@
 package com.jacekpietras.zoo.domain.feature.pathfinder
 
 import com.jacekpietras.zoo.domain.feature.pathfinder.model.Node
+import java.util.PriorityQueue
 
 internal class Dijkstra(
-    vertices: Set<Node>,
+    private val vertices: Set<Node>,
     private val start: Node,
     private val end: Node,
     technicalAllowed: Boolean = false,
@@ -12,42 +13,80 @@ internal class Dijkstra(
     private val previous = mutableMapOf<Node, Node?>()
 
     init {
+        if (technicalAllowed) {
+            withTechnical()
+        } else {
+            withoutTechnical()
+        }
+    }
+
+    private fun withTechnical() {
         // shortest distances
         val delta = mutableMapOf(start to 0.0)
+
+        val q = PriorityQueue(vertices.size / 2, comparator)
+        q.add(start to 0.0)
 
         // subset of vertices, for which we know true distance
         val s = mutableSetOf<Node>()
 
-        var outsideTechnical = technicalAllowed
-
         while (s.size != vertices.size) {
             // closest vertex that has not yet been visited
-            val (v: Node, distanceToV) = delta
-                .asSequence()
-                .filter { !s.contains(it.key) }
-                .minByOrNull(Map.Entry<Node, Double>::value)
-                .let(::checkNotNull)
+            val (v: Node, distanceToV) = q.remove()
 
             v.edges.forEach { neighbor ->
-                if (neighbor.node !in s &&
-                    (technicalAllowed || !outsideTechnical || !neighbor.technical)
-                ) {
-                    if (!neighbor.technical) {
-                        outsideTechnical = true
-                    }
+                if (neighbor.node !in s) {
+                    val newCost = distanceToV + neighbor.weight
 
-                    val newPath = distanceToV + neighbor.weight
-
-                    if (newPath < (delta[neighbor.node] ?: Double.MAX_VALUE)) {
-                        delta[neighbor.node] = newPath
+                    if (newCost < (delta[neighbor.node] ?: Double.MAX_VALUE)) {
+                        delta[neighbor.node] = newCost
                         previous[neighbor.node] = v
+                        q.add(neighbor.node to newCost)
                     }
                 }
             }
 
             if (v == end) break
 
-            v.let(s::add)
+            s.add(v)
+        }
+    }
+
+    private fun withoutTechnical() {
+        // shortest distances
+        val delta = mutableMapOf(start to 0.0)
+
+        val q = PriorityQueue(vertices.size / 2, comparator)
+        q.add(start to 0.0)
+
+        // subset of vertices, for which we know true distance
+        val s = mutableSetOf<Node>()
+
+        var outsideTechnical = false
+
+        while (s.size != vertices.size) {
+            // closest vertex that has not yet been visited
+            val (v: Node, distanceToV) = q.remove()
+
+            v.edges.forEach { neighbor ->
+                if (neighbor.node !in s && (!outsideTechnical || !neighbor.technical)) {
+                    if (!neighbor.technical) {
+                        outsideTechnical = true
+                    }
+
+                    val newCost = distanceToV + neighbor.weight
+
+                    if (newCost < (delta[neighbor.node] ?: Double.MAX_VALUE)) {
+                        delta[neighbor.node] = newCost
+                        previous[neighbor.node] = v
+                        q.add(neighbor.node to newCost)
+                    }
+                }
+            }
+
+            if (v == end) break
+
+            s.add(v)
         }
     }
 
@@ -57,5 +96,12 @@ internal class Dijkstra(
     private fun pathTo(start: Node, end: Node): List<Node> {
         val path = previous[end] ?: return listOf(end)
         return pathTo(start, path) + end
+    }
+
+    private companion object {
+
+        val comparator = Comparator<Pair<Node, Double>> { o1, o2 ->
+            o1.second.compareTo(o2.second)
+        }
     }
 }
