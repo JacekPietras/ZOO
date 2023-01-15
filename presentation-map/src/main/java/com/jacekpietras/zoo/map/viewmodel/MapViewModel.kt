0c3c3da4e433ab982c04ede97697965e46c32629
jacekpietras <jpietras@spoton.com>
2023-01-15 21:50:42 +0100

    fix

--- presentation-map/src/main/java/com/jacekpietras/zoo/map/viewmodel/MapViewModel.kt
+++ presentation-map/src/main/java/com/jacekpietras/zoo/map/viewmodel/MapViewModel.kt
@@ -1,6 +1,7 @@
 package com.jacekpietras.zoo.map.viewmodel
 
 import android.content.Context
+import android.graphics.Paint
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jacekpietras.geometry.PointD
@@ -8,6 +9,7 @@ import com.jacekpietras.mapview.logic.MapViewLogic
 import com.jacekpietras.mapview.model.ComposablePaint
 import com.jacekpietras.mapview.model.RenderItem
 import com.jacekpietras.mapview.ui.compose.ComposablePaintBaker
+import com.jacekpietras.mapview.ui.view.ViewPaintBaker
 import com.jacekpietras.zoo.core.dispatcher.flowOnBackground
 import com.jacekpietras.zoo.core.dispatcher.flowOnMain
 import com.jacekpietras.zoo.core.dispatcher.launchInBackground
@@ -113,16 +115,16 @@ internal class MapViewModel(
     private val getShortestPathUseCase: GetShortestPathFromUserUseCase,
 ) : ViewModel() {
 
-    private val paintBaker = ComposablePaintBaker(context)
+    private val paintBaker = ViewPaintBaker(context)
 
     private val _effects = MutableStateFlow<List<MapEffect>>(emptyList())
     val effects: Flow<Unit> = _effects
         .filter { it.isNotEmpty() }
         .map { /* Unit */ }
 
-    private val mapLogic: MapViewLogic<ComposablePaint> = makeComposableMapLogic()
+    private val mapLogic: MapViewLogic<Paint> = makeComposableMapLogic()
 
-    val mapList = MutableStateFlow<List<RenderItem<ComposablePaint>>>(emptyList())
+    val mapList = MutableStateFlow<List<RenderItem<Paint>>>(emptyList())
 
     private val mapColors = MutableStateFlow(MapColors())
     private val treeBitmap: StateFlow<BitmapVersions?> = flow {
