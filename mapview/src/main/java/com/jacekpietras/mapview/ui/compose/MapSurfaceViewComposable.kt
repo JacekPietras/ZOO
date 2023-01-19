9e366bfd7650b81f66692ae6c526ff99f87c41b6
jacekpietras <jpietras@spoton.com>
2023-01-19 22:43:09 +0100

    profiling

--- mapview/src/main/java/com/jacekpietras/mapview/ui/compose/MapSurfaceViewComposable.kt
+++ mapview/src/main/java/com/jacekpietras/mapview/ui/compose/MapSurfaceViewComposable.kt
@@ -11,7 +11,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
 import com.jacekpietras.mapview.model.RenderItem
+import com.jacekpietras.mapview.ui.LastMapUpdate
+import com.jacekpietras.mapview.ui.LastMapUpdate.cutoE
+import com.jacekpietras.mapview.ui.LastMapUpdate.cutoS
+import com.jacekpietras.mapview.ui.LastMapUpdate.mergE
+import com.jacekpietras.mapview.ui.LastMapUpdate.moveE
+import com.jacekpietras.mapview.ui.LastMapUpdate.rendE
+import com.jacekpietras.mapview.ui.LastMapUpdate.rendS
+import com.jacekpietras.mapview.ui.LastMapUpdate.sortE
+import com.jacekpietras.mapview.ui.LastMapUpdate.sortS
+import com.jacekpietras.mapview.ui.LastMapUpdate.trans
 import com.jacekpietras.mapview.ui.view.MapSurfaceView
+import timber.log.Timber
 
 @Composable
 fun MapSurfaceViewComposable(
@@ -37,4 +48,24 @@ fun MapSurfaceViewComposable(
             }
         },
     )
+
+    val prevRendE = rendE
+    rendE = System.nanoTime()
+    if (trans > 0) {
+
+        Timber.d(
+            "Perf: Render: Full: ${trans toMs rendE}, from prev ${prevRendE toMs rendE}\n" +
+                    "    [pass to vm] ${trans toMs cutoS}\n" +
+                    "    [coord prep] ${cutoS toMs moveE}\n" +
+                    "    [ translate] ${moveE toMs sortS}\n" +
+                    "    [      sort] ${sortS toMs sortE}\n" +
+                    "    [       sum] ${sortE toMs mergE}\n" +
+                    "    [invali req] ${mergE toMs cutoE}\n" +
+                    "    [invalidate] ${cutoE toMs rendS}\n" +
+                    "    [    render] ${rendS toMs rendE}"
+        )
+    }
 }
+
+private infix fun Long.toMs(right: Long) =
+    "${(right - this) / 10_000 / 1_00.0} ms"
