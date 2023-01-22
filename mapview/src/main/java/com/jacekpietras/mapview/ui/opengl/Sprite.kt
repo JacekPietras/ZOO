7d1972ca2b5fea959ce65f1d80f7025d71f0171d
jacekpietras <jpietras@spoton.com>
2023-01-22 23:38:41 +0100

    texture should work but doesn't :)

--- mapview/src/main/java/com/jacekpietras/mapview/ui/opengl/Sprite.kt
+++ mapview/src/main/java/com/jacekpietras/mapview/ui/opengl/Sprite.kt
@@ -2,40 +2,46 @@ package com.jacekpietras.mapview.ui.opengl
 
 import android.graphics.Bitmap
 import android.opengl.GLES20
+import android.opengl.GLUtils
 import com.jacekpietras.mapview.utils.BYTES_PER_FLOAT
 import com.jacekpietras.mapview.utils.COORDS_PER_VERTEX
+import com.jacekpietras.mapview.utils.GL_A_TEX_COORD_VAR
 import com.jacekpietras.mapview.utils.GL_COLOR_VAR
 import com.jacekpietras.mapview.utils.GL_MATRIX_VAR
 import com.jacekpietras.mapview.utils.GL_POSITION_VAR
+import com.jacekpietras.mapview.utils.GL_U_TEX_VAR
+import com.jacekpietras.mapview.utils.GL_V_TEX_COORD_VAR
 import com.jacekpietras.mapview.utils.allocateFloatBuffer
 import com.jacekpietras.mapview.utils.allocateShortBuffer
-import com.jacekpietras.mapview.utils.createGLProgram
+import com.jacekpietras.mapview.utils.loadShader
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.nio.FloatBuffer
 import java.nio.ShortBuffer
 
 internal class Sprite {
 
-    //    private val mCubeTextureCoordinates: FloatBuffer
-//    private var mTextureUniformHandle = 0
-//    private var mTextureCoordinateHandle = 0
-//    private val mTextureCoordinateDataSize = 2
-//    private val vertexShaderCode =
-//        "attribute vec2 $GL_A_TEX_COORD_VAR;" +
-//                "varying vec2 $GL_V_TEX_COORD_VAR;" +
-//                "uniform mat4 $GL_MATRIX_VAR;" +
-//                "attribute vec4 $GL_POSITION_VAR;" +
-//                "void main() {" +
-//                "  gl_Position = $GL_MATRIX_VAR * $GL_POSITION_VAR;" +
-//                "$GL_V_TEX_COORD_VAR = $GL_A_TEX_COORD_VAR;" +
-//                "}"
-//    private val fragmentShaderCode = ("precision mediump float;" +
-//            "uniform vec4 $GL_COLOR_VAR;" +
-//            "uniform sampler2D $GL_U_TEX_VAR;" +
-//            "varying vec2 $GL_V_TEX_COORD_VAR;" +
-//            "void main() {" +
-//            "gl_FragColor = ($GL_COLOR_VAR * texture2D($GL_U_TEX_VAR, $GL_V_TEX_COORD_VAR));" +
-//            "}")
-    private val shaderProgram = createGLProgram()
+    private val mCubeTextureCoordinates: FloatBuffer
+    private var mTextureUniformHandle = 0
+    private var mTextureCoordinateHandle = 0
+    private val mTextureCoordinateDataSize = 2
+    private val vertexShaderCode =
+        "attribute vec2 $GL_A_TEX_COORD_VAR;" +
+                "varying vec2 $GL_V_TEX_COORD_VAR;" +
+                "uniform mat4 $GL_MATRIX_VAR;" +
+                "attribute vec4 $GL_POSITION_VAR;" +
+                "void main() {" +
+                "  gl_Position = $GL_MATRIX_VAR * $GL_POSITION_VAR;" +
+                "$GL_V_TEX_COORD_VAR = $GL_A_TEX_COORD_VAR;" +
+                "}"
+    private val fragmentShaderCode = ("precision mediump float;" +
+            "uniform vec4 $GL_COLOR_VAR;" +
+            "uniform sampler2D $GL_U_TEX_VAR;" +
+            "varying vec2 $GL_V_TEX_COORD_VAR;" +
+            "void main() {" +
+            "gl_FragColor = ($GL_COLOR_VAR * texture2D($GL_U_TEX_VAR, $GL_V_TEX_COORD_VAR));" +
+            "}")
+    private val shaderProgram: Int
 
     init {
 
@@ -44,29 +50,29 @@ internal class Sprite {
         // Because images have a Y axis pointing downward (values increase as you move down the image) while
         // OpenGL has a Y axis pointing upward, we adjust for that here by flipping the Y axis.
         // What's more is that the texture coordinates are the same for every face.
-//        val cubeTextureCoordinateData = floatArrayOf(
-//            -0.5f, 0.5f,
-//            -0.5f, -0.5f,
-//            0.5f, -0.5f,
-//            0.5f, 0.5f
-//        )
-//        mCubeTextureCoordinates = ByteBuffer.allocateDirect(cubeTextureCoordinateData.size * 4)
-//            .order(ByteOrder.nativeOrder())
-//            .asFloatBuffer()
-//        mCubeTextureCoordinates.put(cubeTextureCoordinateData).position(0)
+        val cubeTextureCoordinateData = floatArrayOf(
+            -0.5f, 0.5f,
+            -0.5f, -0.5f,
+            0.5f, -0.5f,
+            0.5f, 0.5f,
+        )
+        mCubeTextureCoordinates = ByteBuffer.allocateDirect(cubeTextureCoordinateData.size * 4)
+            .order(ByteOrder.nativeOrder())
+            .asFloatBuffer()
+        mCubeTextureCoordinates.put(cubeTextureCoordinateData).position(0)
 
-//        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
-//        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
-//        shaderProgram = GLES20.glCreateProgram()
-//        GLES20.glAttachShader(shaderProgram, vertexShader)
-//        GLES20.glAttachShader(shaderProgram, fragmentShader)
+        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+        shaderProgram = GLES20.glCreateProgram()
+        GLES20.glAttachShader(shaderProgram, vertexShader)
+        GLES20.glAttachShader(shaderProgram, fragmentShader)
 
-//        GLES20.glBindAttribLocation(shaderProgram, 0, GL_A_TEX_COORD_VAR)
-//        GLES20.glLinkProgram(shaderProgram)
+        GLES20.glBindAttribLocation(shaderProgram, 0, GL_A_TEX_COORD_VAR)
+        GLES20.glLinkProgram(shaderProgram)
     }
 
     fun draw(mvpMatrix: FloatArray?, cX: Float, cY: Float, bitmap: Bitmap) {
-//        val mTextureDataHandle = loadTexture(bitmap)
+        val mTextureDataHandle = loadTexture(bitmap)
         val data = SquareShapeData(cX, cY, bitmap.height.toFloat(), bitmap.width.toFloat())
 
         GLES20.glUseProgram(shaderProgram)
@@ -86,29 +92,29 @@ internal class Sprite {
         )
 
         //Set Texture Handles and bind Texture
-//        mTextureUniformHandle = GLES20.glGetAttribLocation(shaderProgram, GL_U_TEX_VAR)
-//        mTextureCoordinateHandle = GLES20.glGetAttribLocation(shaderProgram, GL_A_TEX_COORD_VAR)
+        mTextureUniformHandle = GLES20.glGetAttribLocation(shaderProgram, GL_U_TEX_VAR)
+        mTextureCoordinateHandle = GLES20.glGetAttribLocation(shaderProgram, GL_A_TEX_COORD_VAR)
 
         //Set the active texture unit to texture unit 0.
-//        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
 
         //Bind the texture to this unit.
-//        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureDataHandle)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureDataHandle)
 
         //Tell the texture uniform sampler to use this texture in the shader by binding to texture unit 0.
-//        GLES20.glUniform1i(mTextureUniformHandle, 0)
+        GLES20.glUniform1i(mTextureUniformHandle, 0)
 
         //Pass in the texture coordinate information
-//        mCubeTextureCoordinates.position(0)
-//        GLES20.glVertexAttribPointer(
-//            mTextureCoordinateHandle,
-//            mTextureCoordinateDataSize,
-//            GLES20.GL_FLOAT,
-//            false,
-//            0,
-//            mCubeTextureCoordinates
-//        )
-//        GLES20.glEnableVertexAttribArray(mTextureCoordinateHandle)
+        mCubeTextureCoordinates.position(0)
+        GLES20.glVertexAttribPointer(
+            mTextureCoordinateHandle,
+            mTextureCoordinateDataSize,
+            GLES20.GL_FLOAT,
+            false,
+            0,
+            mCubeTextureCoordinates
+        )
+        GLES20.glEnableVertexAttribArray(mTextureCoordinateHandle)
 
         val mColorHandle = GLES20.glGetUniformLocation(shaderProgram, GL_COLOR_VAR)
         GLES20.glUniform4fv(mColorHandle, 1, color, 0)
@@ -125,28 +131,25 @@ internal class Sprite {
         GLES20.glDisableVertexAttribArray(mPositionHandle)
     }
 
-//    private fun loadTexture(bitmap: Bitmap): Int {
-//        val textureHandle = IntArray(1)
-//        GLES20.glGenTextures(1, textureHandle, 0)
-//        if (textureHandle[0] != 0) {
-//            val options = BitmapFactory.Options()
-//            options.inScaled = false // No pre-scaling
-//
-//            // Bind to the texture in OpenGL
-//            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0])
-//
-//            // Set filtering
-//            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST)
-//            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST)
-//
-//            // Load the bitmap into the bound texture.
-//            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
-//        }
-//        if (textureHandle[0] == 0) {
-//            throw RuntimeException("Error loading texture.")
-//        }
-//        return textureHandle[0]
-//    }
+    private fun loadTexture(bitmap: Bitmap): Int {
+        val textureHandle = IntArray(1)
+        GLES20.glGenTextures(1, textureHandle, 0)
+        if (textureHandle[0] != 0) {
+            // Bind to the texture in OpenGL
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0])
+
+            // Set filtering
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST)
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST)
+
+            // Load the bitmap into the bound texture.
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
+        }
+        if (textureHandle[0] == 0) {
+            throw RuntimeException("Error loading texture.")
+        }
+        return textureHandle[0]
+    }
 
     private class SquareShapeData(cX: Float, cY: Float, height: Float, width: Float) : ShapeOfTrianglesData(color) {
 
