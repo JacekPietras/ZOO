3d1648c40920fba8854c4adb5b45337ad5d98896
jacekpietras <jpietras@spoton.com>
2023-01-29 19:33:37 +0100

    cleanup

--- mapview/src/main/java/com/jacekpietras/mapview/utils/CollectionUtils.kt
+++ mapview/src/main/java/com/jacekpietras/mapview/utils/CollectionUtils.kt
@@ -1,5 +1,7 @@
 package com.jacekpietras.mapview.utils
 
+import com.jacekpietras.geometry.PointD
+
 internal fun Collection<Int>.toShortArray(): ShortArray {
     val result = ShortArray(size)
     var index = 0
@@ -7,3 +9,15 @@ internal fun Collection<Int>.toShortArray(): ShortArray {
         result[index++] = element.toShort()
     return result
 }
+
+fun pointsToDoubleArray(list: List<PointD>): DoubleArray {
+    val result = DoubleArray(list.size * 2)
+    for (i in list.indices) {
+        result[i shl 1] = list[i].x
+        result[(i shl 1) + 1] = list[i].y
+    }
+    return result
+}
+
+fun List<PointD>.toDoubleArray(): DoubleArray =
+    pointsToDoubleArray(this)
--- mapview/src/main/java/com/jacekpietras/mapview/utils/MapUtils.kt
+++ mapview/src/main/java/com/jacekpietras/mapview/utils/MapUtils.kt
@@ -2,16 +2,6 @@ package com.jacekpietras.mapview.utils
 
 import android.animation.ValueAnimator
 import android.view.animation.AccelerateDecelerateInterpolator
-import com.jacekpietras.geometry.PointD
-
-fun pointsToDoubleArray(list: List<PointD>): DoubleArray {
-    val result = DoubleArray(list.size * 2)
-    for (i in list.indices) {
-        result[i shl 1] = list[i].x
-        result[(i shl 1) + 1] = list[i].y
-    }
-    return result
-}
 
 fun doAnimation(onUpdate: (progress: Float) -> Unit) {
     ValueAnimator.ofFloat(1f)
